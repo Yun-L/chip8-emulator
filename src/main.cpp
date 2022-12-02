353f57ae49283c3f756c5e34404197a87cf13fa5
Yun-L <yun26603020@gmail.com>
2022-12-02 00:22:17 -0500

    chip 8 memory layout

--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,20 @@ int main(int argc, char *argv[])
 #define NIBBLE_4(ins) ((uint8_t)(ins & 0x000F))
 #define NIBBLE_234(ins) (ins & 0x0FFF)
 #define NIBBLE_34(ins) ((uint8_t)(ins & 0x00FF))
+
+// TODO: replace with cli arg
+std::string INPUT_ROM = "../data/IBM Logo.ch8";
+
+uint8_t registers[16] = {0};
+uint8_t stack_p = 0;
+uint8_t delay_timer = 0;
+uint8_t sound_timer = 0;
+uint16_t index = 0;
+uint16_t pc = 0x200;
+
+uint8_t ram[4096] = {0};
+uint8_t stack[64] = {0};
+
     printf("test");
     return 0;
 }
