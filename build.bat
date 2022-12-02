24dfcb8637ae4799f94aabf6836dad617adf7b35
Yun-L <yun26603020@gmail.com>
2022-12-02 00:19:58 -0500

    compile with debug info

--- build.bat
+++ build.bat
@@ -2,16 +2,6 @@
 pushd "%~dp0"
 mkdir .\build
 pushd .\build
-cl ..\src\main.cpp
-rem cl /I ..\..\SDL2-2.24.1\include\ ^
-rem    /I ..\..\SDL2_image-2.6.2\include\ ^
-rem /link ^
-rem    /LIBPATH:..\..\SDL2-2.24.1\lib\x64\ ^
-rem    /LIBPATH:..\..\SDL2_image-2.6.2\lib\x64\ ^
-rem    /SUBSYSTEM:WINDOWS ^
-rem    shell32.lib SDL2.lib SDL2main.lib SDL2_image.lib
-rem copy DLL's to executable directory
-rem xcopy /Y ..\..\SDL2-2.24.1\lib\x64\SDL2.dll .
-rem xcopy /Y ..\..\SDL2_image-2.6.2\lib\x64\SDL2_image.dll .
+cl /Zi ..\src\main.cpp
 popd
 popd
