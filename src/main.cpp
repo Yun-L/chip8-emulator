c171743d3c3e4e487611c7bfc65f328b38e3a10c
Yun-L <yun26603020@gmail.com>
2022-12-02 00:21:39 -0500

    macros to get necessary segments from full instruction

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,12 @@
 
 int main(int argc, char *argv[])
 {
+#define NIBBLE_1(ins) ((uint8_t)(ins >> 12))
+#define NIBBLE_2(ins) ((uint8_t)(ins >> 8) & 0x0F)
+#define NIBBLE_3(ins) ((uint8_t)(ins >> 4) & 0x0F)
+#define NIBBLE_4(ins) ((uint8_t)(ins & 0x000F))
+#define NIBBLE_234(ins) (ins & 0x0FFF)
+#define NIBBLE_34(ins) ((uint8_t)(ins & 0x00FF))
     printf("test");
     return 0;
 }
